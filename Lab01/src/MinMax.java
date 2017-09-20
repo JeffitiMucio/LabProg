dd0e3fc98264501d73801546f9ddbd072289a583
SAMSUNG <SAMSUNG@LAPTOP-PQ6IBU8P>
2017-09-19 21:24:48 -0300

    Primeiro programa

---
+++ Lab01/src/MinMax.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class MinMax {
+	public static void main (String[] args) {
+		final int NÚMEROS_A_LER = 3;
+		Scanner sc = new Scanner (System.in);
+		int minimo = Integer.MAX_VALUE;
+		int máximo = Integer.MIN_VALUE;
+		
+		for (int i = 0;i < NÚMEROS_A_LER; i++) {
+			System.out.print("Entre com proximo inteiro:");
+			int num = sc.nextInt();
+			if (num< minimo) {
+				minimo = num;
+			}
+		    if (num > máximo) {
+		    	máximo = num;
+		    	
+		    }
+			
+		  
+		}
+		
+		System.out.println("O menor numero é: "+ minimo);
+		System.out.println("O menor numero é: "+ máximo);
+	}
+}
