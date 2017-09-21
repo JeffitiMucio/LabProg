5c5cbc4af4b2a05bf17ac2fe14854290f34822ef
JeffitiMucio <32114161+JeffitiMucio@users.noreply.github.com>
2017-09-21 14:40:31 -0300

    Update MinMax.java

--- Lab01/src/MinMax.java
+++ Lab01/src/MinMax.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 
 public class MinMax {
 	public static void main (String[] args) {
-		final int N⁄MEROS_A_LER = 3;
+		final int N√öMEROS_A_LER = 3;
 		Scanner sc = new Scanner (System.in);
 		int minimo = Integer.MAX_VALUE;
-		int m·ximo = Integer.MIN_VALUE;
+		int m√°ximo = Integer.MIN_VALUE;
 		
-		for (int i = 0;i < N⁄MEROS_A_LER; i++) {
+		for (int i = 0;i < N√öMEROS_A_LER; i++) {
 			System.out.print("Entre com proximo inteiro:");
 			int num = sc.nextInt();
 			if (num< minimo) {
 				minimo = num;
 			}
-		    if (num > m·ximo) {
-		    	m·ximo = num;
+		    if (num > m√°ximo) {
+		    	m√°ximo = num;
 		    	
 		    }
 			
 		  
 		}
 		
-		System.out.println("O menor numero È: "+ minimo);
-		System.out.println("O menor numero È: "+ m·ximo);
+		System.out.println("O menor numero √©: "+ minimo);
+		System.out.println("O maior numero √©: "+ m√°ximo);
 	}
 }
